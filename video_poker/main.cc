17966d791891038aa74a0ded0e6e658fd834e068
Nathan <nathankerr96@gmail.com>
2025-09-06 23:06:48 -0700

    Add a Couple Simple Runs of 170-170-Softmax

--- video_poker/main.cc
+++ video_poker/main.cc
@@ -13,7 +13,7 @@
 #include <ctime>
 
 #define INPUT_SIZE 85
-#define LEARNING_RATE 0.001
+#define LEARNING_RATE 0.002
 #define EVAL_ITERATIONS 100000
 #define LOGS_DIR "logs/"
 #define LOG_NAME  "170-170-Softmax"
